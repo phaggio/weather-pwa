abb5a5d1751683feda4fa8e36396f3ee1333abc4
phaggio <richardchacha@hotmail.com>
2020-06-08 21:44:09 -0700

    re-arrange current weather div

--- client/src/components/CurrentWeatherDiv/index.js
+++ client/src/components/CurrentWeatherDiv/index.js
@@ -11,19 +11,26 @@ const CurrentWeatherDiv = props => {
   const appContext = useContext(AppContext);
   return (
     <Row>
+      <Col size="4 sm-4 lg-3">
+        <img size="w-100" src={require(`../../assets/${props.currentWeather.weather[0].icon}@2x.png`)} alt="weather icon" />
+      </Col>
       <Col size="8 sm-8 lg-9">
-        <h2>{props.currentWeather.name}</h2>
-        <h6>{currentCountryName}</h6>
+
+        <div className="d-flex">
+          <h1 className="display-3">{props.currentWeather.main.temp}</h1><p className="mt-3">{appContext.units}</p>
+        </div>
+
+        <div className="d-flex align-items-end">
+          <h2 className="display-4">{props.currentWeather.name}</h2><small className="ml-2">{currentCountryName}</small>
+        </div>
+
         <p>Sunrise: {Moment.unix(props.currentWeather.sys.sunrise).format('h:mm A')}</p>
         <p>Sunset: {Moment.unix(props.currentWeather.sys.sunset).format('h:mm A')}</p>
         <p>Description: {props.currentWeather.weather[0].description}</p>
-        <p>Current temperature: {props.currentWeather.main.temp}{appContext.units}</p>
+
         <p>Feels like: {props.currentWeather.main.feels_like}{appContext.units}</p>
         <p>Humidity: {props.currentWeather.main.humidity}%</p>
         <p>Pressure: {props.currentWeather.main.pressure} hPa</p>
-      </Col>
-      <Col size="4 sm-4 lg-3">
-        <img size="w-100" src={require(`../../assets/${props.currentWeather.weather[0].icon}@2x.png`)} alt="weather icon" />
       </Col>
     </Row>
 
