d6079e0a75596c35f33ea89549f45af66459c5c6
phaggio <richardchacha@hotmail.com>
2020-06-08 22:42:49 -0700

    Updates

---
+++ precache-manifest.0ffdf7c08fac92b64bbd5ffabd3bf6b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48649d30f4c66fd202b522218cbbe8fc",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "5bf42f15fe035fad4f58",
+    "url": "/weather-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3abd932f6972262a2af1",
+    "url": "/weather-app/static/js/2.96733eb0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/weather-app/static/js/2.96733eb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bf42f15fe035fad4f58",
+    "url": "/weather-app/static/js/main.771b83f1.chunk.js"
+  },
+  {
+    "revision": "ea6a87d0551586d0c7b5",
+    "url": "/weather-app/static/js/runtime-main.34dd0968.js"
+  }
+]);
