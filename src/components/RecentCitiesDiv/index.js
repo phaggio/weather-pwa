dbce380799c9fad3f3c142de819d5278ccbe60af
phaggio <richardchacha@hotmail.com>
2020-06-13 00:33:59 -0700

    darkMode to RecentCities

--- src/components/RecentCitiesDiv/index.js
+++ src/components/RecentCitiesDiv/index.js
@@ -1,19 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import ThemeContext from '../../utils/ThemeContext';
 
 const RecentCitiesDiv = props => {
+  const themeContext = useContext(ThemeContext);
+
   return (
-    <div className="d-flex flex-column btn-group mt-3">
-      <label>Recent cities</label>
+    <div className={`d-flex flex-column btn-group mt-3 bg-${themeContext.backgroundColor}`}>
+      <label className={`text-${themeContext.textColor}`}>Recent cities</label>
       <ul className="list-group w-100">
         {props.recentCities.map(city => {
           return (
             <div className="btn-group rounded" key={city.key}>
-              <button className="btn btn-light w-75"
+              <button className={`btn btn-${themeContext.backgroundColor} w-75`}
                 value={city}
                 onClick={() => props.recentCityButtonPressed({ city: city.city, country: city.country })} >
                 {`${city.city}, ${city.country}`}
               </button>
-              <button className="btn btn-light w-25"
+              <button className={`btn btn-${themeContext.backgroundColor} w-25`}
                 value={city}
                 onClick={() => props.removeCityButtonPressed(city.key)}>
                 <i className="material-icons">delete_outline</i>
