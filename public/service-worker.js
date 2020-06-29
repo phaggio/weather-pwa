05cb8fe4afbc330f009ac1732d9c864a1810b29e
phaggio <richardchacha@hotmail.com>
2020-06-28 23:03:38 -0700

    enable serviceWorker

--- public/service-worker.js
+++
@@ -1,7 +0,0 @@
-console.log(`Hello from your service worker!`);
-
-const FILES_TO_CACHE = [
-  '/',
-  '/home',
-  `/manifest.webmanifest.json`
-];
--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Test', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(`/simple-weather`);
   expect(linkElement).toBeInTheDocument();
 });
