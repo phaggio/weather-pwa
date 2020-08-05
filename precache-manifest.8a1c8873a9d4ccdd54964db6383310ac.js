f9e96d082069b24a21a90b2da45f3a762e66d8c3
phaggio <richardchacha@hotmail.com>
2020-08-05 15:54:16 -0700

    Updates

---
+++ precache-manifest.8a1c8873a9d4ccdd54964db6383310ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "746d62cf1c07757b3b7b808ad8d4c0de",
+    "url": "/weather-pwa/index.html"
+  },
+  {
+    "revision": "e9ad4f1451074535b5cf",
+    "url": "/weather-pwa/static/js/2.c1aac722.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/weather-pwa/static/js/2.c1aac722.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ed377377d1f43ef4e8a",
+    "url": "/weather-pwa/static/js/main.9d6b51d1.chunk.js"
+  },
+  {
+    "revision": "1c4f3d0bd6aeef4eaebb",
+    "url": "/weather-pwa/static/js/runtime-main.e7aa6856.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/simple-weather/precache-manifest.a5a82412ee957947f58787d57ae0065d.js"
+  "/weather-pwa/precache-manifest.8a1c8873a9d4ccdd54964db6383310ac.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/simple-weather/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/weather-pwa/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
